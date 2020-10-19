e7f081d2778db3d1f8d9ab69a06b9167ff4aa3a0
Ashlie Gray <ashliejanegray@gmail.com>
2020-10-19 20:24:20 +1000

    initial commit of un-used files

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'articles#index'
+
+  resources :articles
+  #^^ this line replaces everything below!
+  # get '/articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new', as: :new_article
+  # get '/articles/:id', to: 'articles#show', as: :article
+  # post '/articles', to: 'articles#create'
+  # get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch '/articles/:id', to: 'articles#update'
+  # delete '/articles/:id', to: 'articles#destroy'
+end
